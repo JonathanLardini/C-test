1893f29888f7fa782dc72c3b72f54dd50626814b
JonathanLardini <lardinijonathan10072002@gmail.com>
2026-05-18 16:57:00 +0200

    Add files via upload

---
+++ Personnage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Personnage
+{
+    delegate void DelegateType(string text);
+    delegate void DelegateType2(string text);
+    void Maj(string text)
+    {
+        Console.WriteLine(text.ToUpper());
+    }
+    void Min(string text)
+    {
+        Console.WriteLine(text.ToLower());
+    }
+    public void AfficherText()
+    {
+        DelegateType variableDelegate = Maj;
+        DelegateType2 variableDelegate2 = Min;
+        variableDelegate("Salut");
+        variableDelegate2("Salut");
+    }
+}
