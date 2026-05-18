85256e1abd55b3336faa9432871bcbbec2d85561
JonathanLardini <lardinijonathan10072002@gmail.com>
2026-05-18 18:12:16 +0200

    Update with_delegate.cs

--- with_delegate.cs
+++ with_delegate.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 class Test
 {
+    delegate void MyDelegate(string text);
+    delegate void MyDelegate2(string text);
     void Maj(string text)
     {
         Console.WriteLine(text.ToUpper());
@@ -14,7 +16,9 @@ class Test
     }
     public void AfficherText()
     {
-        Maj("Salut");
-        Min("Salut");
+        MyDelegate variableDelegate=Maj;
+        variableDelegate("Salut");
+        MyDelegate2 variableDelegate2=Maj;
+        variableDelegate2("Salut");
     }
 }
